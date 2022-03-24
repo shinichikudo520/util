99a8a9e4598bccf1a7746430c62d69e7cbaaafc5
unknown <jlc_0070@SZ-JLC.COM>
2022-03-24 15:48:07 +0800

    reset

--- instanceExecute.ts
+++ instanceExecute.ts
@@ -1,5 +1,5 @@
 /**
- * 处理并发的promise，让promise排队处理
+ * 处理并发的promise
  * @param executor 排队处理的异步函数
  * @param immediately 立即执行
  * @returns
@@ -9,16 +9,16 @@ export function instanceExecute(
   immediately = false
 ) {
   let promise: Promise<any> = null;
-  if (immediately) {
+  if (immediately) { // immediately立即执行一次
     return (...args) => executor(...args);
   } else {
     return (...args) => {
-      if (!promise || immediately) {
+      if (!promise || immediately) { // immediately立即执行一次
         promise = executor(...args).finally(() => {
           promise = null;
         });
       }
-      return promise;
+      return promise; // 短时间内重复调用同一个promise，而上一次调用还没执行完成，则不重复调用，直接返回上一次的调用
     };
   }
 }
