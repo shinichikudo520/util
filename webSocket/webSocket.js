2bdaf9c284e92577c5e377cd3053796ca9d478a6
unknown <jlc_0070@SZ-JLC.COM>
2022-03-24 15:26:00 +0800

    修改注释

---
+++ webSocket/webSocket.js
@@ -0,0 +1,9 @@
+// 1. 构建 webSocket 对象
+var WebSocket1 = /** @class */ (function () {
+    function WebSocket1(url) {
+    }
+    return WebSocket1;
+}());
+/*****------------------------------- */
+var ws1 = new WebSocket1('ws://localhost:8080');
+// ws1.showMsg();
