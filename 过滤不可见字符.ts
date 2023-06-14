36e997119becd5136678c337c47900b305204c91
unknown <jlc_0226@SZ-JLC.COM>
2023-06-14 18:13:38 +0800

    过滤不可见字符

---
+++ 过滤不可见字符.ts
@@ -0,0 +1,19 @@
+/** 正则过滤: 不可见字符范围正则表达式 [\x00-\x1F\x7F] */
+function filterInvisibleChar(str: string) {
+  return str.replace(/[\x00-\x1F\x7F]+/g, ``);
+}
+/** 字符编号过滤: 不可见字符编码 code: 1-31 或者 127 */
+function filterInvisibleChar1(str: string) {
+  let result = "";
+  if (str) {
+    for (let i = 0; i < str.length; i++) {
+      const code = str.charCodeAt(i);
+      if ((code >= 0 && code <= 31) || code === 127) {
+        continue;
+      } else {
+        result += str[i];
+      }
+    }
+  }
+  return result.trim();
+}
